eeaa22f46c7848313d9ecc514228649c2db7c31c
keziahNsoh <porepeya@gmail.com>
2024-05-21 22:53:18 -0700

    1-repetition_token_0.rb

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,12 @@
+# Define the regular expression
+pattern = /School/
+
+# Accept one argument from the command line
+input = ARGV[0]
+
+# Check if the input matches the pattern
+if input.match?(pattern)
+  puts "The input '#{input}' matches the pattern."
+else
+  puts "The input '#{input}' does not match the pattern."
+end
