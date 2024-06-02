f873795600c25718a94c9eadf04d9f3eacf8bb8d
keziahNsoh <porepeya@gmail.com>
2024-06-01 20:25:03 -0700

    5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -3,13 +3,10 @@
 # Regular expression to match the pattern (case-insensitive)
 regex = /^h.n$/i
 
-# Accept the argument from command line
 input = ARGV[0]
 
-# Match the input against the regular expression
 match = input.match(regex)
 
-# Output the result
 if match
   puts match[0]
 else
