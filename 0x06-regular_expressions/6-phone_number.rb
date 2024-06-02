dc85217c3cb03cd954d4a065946a13c5e69230ee
keziahNsoh <porepeya@gmail.com>
2024-06-01 20:20:16 -0700

    6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -3,15 +3,12 @@
 # Regular expression to match a 10-digit phone number
 regex = /^\d{10}$/
 
-# Accept the argument from command line
 input = ARGV[0]
 
-# Match the input against the regular expression
 match = input.match(regex)
 
-# Output the result
 if match
   puts match[0]
 else
-  puts "No match found"
+  puts ""
 end
