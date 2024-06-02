cf3ae9b2ed806aaf37f2566f6a62ffe2b0472d93
keziahNsoh <porepeya@gmail.com>
2024-06-01 20:09:28 -0700

    6-phone_number.rb

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# Regular expression to match a 10-digit phone number
+regex = /^\d{10}$/
+
+# Accept the argument from command line
+input = ARGV[0]
+
+# Match the input against the regular expression
+match = input.match(regex)
+
+# Output the result
+if match
+  puts match[0]
+else
+  puts "No match found"
+end
