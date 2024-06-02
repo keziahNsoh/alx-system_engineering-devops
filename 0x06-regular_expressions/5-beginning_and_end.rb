688eda3f79fc041387b998227f35d8f0a6a47bd0
keziahNsoh <porepeya@gmail.com>
2024-06-01 19:44:03 -0700

    2-repetition_token_1.rb  3-repetition_token_2.rb  4-repetition_token_3.rb  5-beginning_and_end.rb

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# Regular expression to match the pattern
+regex = /h.n/
+
+# Accept the argument from command line
+input = ARGV[0]
+
+# Match the input against the regular expression
+match = input.match(regex)
+
+# Output the result
+if match
+  puts match[0]
+else
+  puts "No match found"
+end
