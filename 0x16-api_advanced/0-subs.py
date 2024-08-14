b53cc0c9aa540dd5caea9843d2a5027268ae1a33
keziahNsoh <porepeya@gmail.com>
2024-08-13 17:12:42 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,14 +14,23 @@ def number_of_subscribers(subreddit):
         int: Number of subscribers if subreddit is valid, 0 otherwise.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": ("python:example:v1.0 (by /u/yourusername)")}
+    headers = {
+        "User-Agent":
+        "python:subreddit_subscriber_count:v1.0 (by /u/yourusername)"
+    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data["data"]["subscribers"]
-        else:
+        elif response.status_code == 302:  # Redirect status code
+            # The subreddit may be invalid if we get a
+            # redirect to a search page
             return 0
-    except Exception:
+        else:
+            # For any other status codes, consider it as invalid
+            return 0
+    except requests.RequestException:
+        # Catch any requests-related exceptions and return 0
         return 0
