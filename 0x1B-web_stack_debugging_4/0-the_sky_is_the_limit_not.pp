5f7ab4330f9f500215e50fb13faea33352676b66
keziahNsoh <porepeya@gmail.com>
2024-08-24 17:55:04 -0700

    first commit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,29 +1,13 @@
 # This manifest configures the Nginx web server with basic settings.
-
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "server {
-      listen 80;
-      server_name localhost;
-
-      location / {
-        root /var/www/html;
-        index index.html index.htm;
-      }
-    }",
-    notify  => Service['nginx'],
-  }
+# # sets file limite for nginx to 4096
+# Usage example: puppet apply 0-the_sky_is_the_limit_not.pp
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-include nginx
-
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
